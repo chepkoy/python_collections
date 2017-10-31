c7c3263622d3c4ba3f623bc2930c0b54d230e74e
chepkoy <invalskies12@gmail.com>
2017-11-01 00:53:08 +0300

    shopping list 3

---
+++ lists/shopping_list3.py
@@ -0,0 +1,69 @@
+import os
+
+shopping_list = []
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+
+def show_help():
+    clear_screen()
+    # Print instructions on how to use the app
+    print("What should we pick at the store? ")
+    print("""
+Enter 'DONE' to stop adding items.
+Enter 'HELP' for this Help.
+Enter 'SHOW' to see your current list.
+""")
+def show_list():
+    clear_screen()
+    # Print out the list
+    print("here is your shopping list: ")
+
+    index = 1
+    for item in shopping_list:
+        print("{}. {}".format(index, item))
+        index += 1
+    print("-"*10)
+
+def add_to_list(item):
+    show_list()
+    if len(shopping_list):
+        position = input("Where should i add {}?\n"
+                         "Press ENTER to add to the end of the list\n"
+                         "> ".format(item))
+    else:
+        position = 0
+
+    try:
+        position = abs(int(position))
+    except ValueError:
+        position = None
+    if position is not None:
+        shopping_list.insert(position-1, item)
+    else:
+        shopping_list.append(new_item)
+    show_list()
+
+
+
+
+show_help()
+
+while True:
+    # Ask for new items
+    new_item = input("> ")
+
+    # Be able to quit the app
+    if new_item.upper() == "DONE" or new_item.upper() == "QUIT":
+        break
+    elif new_item.upper() == "HELP":
+        show_help()
+        continue
+    elif new_item.upper() == 'SHOW':
+        show_list()
+        continue
+    else:
+        add_to_list(new_item)
+
+show_list()
