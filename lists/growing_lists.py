4989812bef622a9a9b58d4366398019a74b3ec0c
chepkoy <invalskies12@gmail.com>
2017-11-01 00:20:10 +0300

    making lists bigger

---
+++ lists/growing_lists.py
@@ -0,0 +1,21 @@
+# Adding lists together
+favourite_subjects = ['math','geography', 'physics']
+print(favourite_subjects + ['Biochemistry'])
+
+# Using append
+favourite_subjects.append("zoology")
+
+print(favourite_subjects)
+
+# Using extend
+favourite_subjects.extend(["quantum chemistry", "Physical chemistry"])
+
+print(favourite_subjects)
+
+# Using insert
+
+favourite_subjects.insert(1, 'botany')
+print(favourite_subjects)
+
+
+
