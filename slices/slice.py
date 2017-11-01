6514f99e8d7123f4effd68639a705fa291a04fc6
chepkoy <invalskies12@gmail.com>
2017-11-01 10:24:35 +0300

    Working with slices

---
+++ slices/slice.py
@@ -0,0 +1,12 @@
+# Slicing items from a list with indexes
+list =['allan','naomi','brian','kevin']
+
+print(list[1:2])
+
+# The last and the first item in the list
+print(list[:2])
+print(list[2:])
+
+# Getting back a copy of the list
+
+print(list[:])
