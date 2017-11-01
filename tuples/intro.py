19f935a5d6e68b060786828ac3dde3bea9b09948
chepkoy <invalskies12@gmail.com>
2017-11-01 14:43:10 +0300

    Tuple introduction

---
+++ tuples/intro.py
@@ -0,0 +1,16 @@
+# The role oc commas in a tuple
+
+my_tuple = (1, 2, 3)
+print(my_tuple)
+
+my_second_tuple = 1, 2, 3, 4
+print(my_second_tuple)
+
+my_third_tuple = (3,)
+print(my_third_tuple)
+
+# Creating tuples with the tuple function
+
+my_fourth_tuple =tuple([1, 2, 3])
+
+print(my_fourth_tuple)
