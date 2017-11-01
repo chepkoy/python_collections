525c01dad077d58d7221edb034158b2c0906c2d3
chepkoy <invalskies12@gmail.com>
2017-11-01 11:03:18 +0300

    Introduction to dictionaries

---
+++ dictionaries/dict.py
@@ -0,0 +1,17 @@
+# Working with dictionaries
+
+course = {'Title':"python basics"}
+print(course)
+print(course['Title'])
+
+# Using the dict function
+
+name  = dict([["name","allan"]])
+print(name)
+
+#
+
+tutorial = {'title':'python collections','teacher':{'first_name':'Kenneth', 'last_name':'love'}}
+
+print(tutorial['teacher'])
+print(tutorial['teacher']['first_name'])
