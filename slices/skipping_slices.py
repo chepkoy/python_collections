a85c4bc9a2270cba79b85024da4396d472f344c4
chepkoy <invalskies12@gmail.com>
2017-11-01 10:38:01 +0300

    Using start stop and step

---
+++ slices/skipping_slices.py
@@ -0,0 +1,11 @@
+# Using step
+numbers = list(range(20))
+print(numbers[::2])
+
+# Using negative indexes
+print(numbers[-2:])
+
+# Moving backwards
+
+print(numbers[-2:-5:-1])
+print(numbers[::-1])
