0b735f90a777aae0c86f93e53ea0919230510388
chepkoy <invalskies12@gmail.com>
2017-11-01 16:44:53 +0300

    Introduction to sets

---
+++ sets/set_intro.py
@@ -0,0 +1,17 @@
+# Creating a set
+
+my_set = {3, 4, 5, 6, 8}
+print(my_set)
+
+# Single item in a  set use the set function
+
+my_single_item = set([1])
+print(my_single_item)
+
+# adding item int sets
+
+my_set.add(10)
+print(my_set)
+
+my_set.update({1,2,7,9})
+print(my_set)
